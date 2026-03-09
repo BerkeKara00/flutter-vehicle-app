055688f5f7f510ea221d02ea9a4b0754213360c8
Berke Kara <kberkek00@gmail.com>
2026-03-09 18:58:43 +0300

    Display vehicles on home screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -10,13 +10,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-TextEditingController searchController = TextEditingController();
-bool isLoading = false;
-String errorMessage = "";
-List<Data> allVehicles = [];
-ApiService apiService = ApiService();
+  TextEditingController searchController = TextEditingController();
+  bool isLoading = false;
+  String errorMessage = "";
+  List<Data> allVehicles = [];
+  ApiService apiService = ApiService();
 
-@override
+  @override
   void initState() {
     loadVehicles();
     super.initState();
@@ -32,14 +32,13 @@ ApiService apiService = ApiService();
       setState(() {
         allVehicles = resData.data ?? [];
       });
-
     } catch (e) {
       setState(() {
         errorMessage = "Failed to load vehicles data.";
       });
-    }finally{
+    } finally {
       setState(() {
-        isLoading=false;
+        isLoading = false;
       });
     }
   }
@@ -57,34 +56,33 @@ ApiService apiService = ApiService();
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("Discover", style: TextStyle(
-                    fontSize: 34,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: -0.5,
+                  Text(
+                    "Discover",
+                    style: TextStyle(
+                      fontSize: 34,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: -0.5,
+                    ),
                   ),
-                ),
-                IconButton(
-                  onPressed: (){}, 
-                  icon: Icon(Icons.shopping_bag_outlined),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.shopping_bag_outlined),
                   )
                 ],
               ),
               SizedBox(height: 8),
-          
               Text(
-                  "Discover our latest vehicles.",
-                  style: TextStyle(
+                "Discover our latest vehicles.",
+                style: TextStyle(
                   fontSize: 18,
                   color: Colors.grey,
                 ),
               ),
-          
               SizedBox(height: 14),
-          
               Container(
                 decoration: BoxDecoration(
                   color: Color(0xfff5f5f5),
-                  borderRadius: BorderRadius.circular(12), 
+                  borderRadius: BorderRadius.circular(12),
                 ),
                 child: TextField(
                   controller: searchController,
@@ -92,35 +90,144 @@ ApiService apiService = ApiService();
                     hintText: "Search vehicles",
                     hintStyle: TextStyle(color: Colors.grey),
                     border: InputBorder.none,
-                    prefixIcon: Icon(Icons.search, color:Colors.grey),
+                    prefixIcon: Icon(Icons.search, color: Colors.grey),
                     contentPadding: EdgeInsets.symmetric(vertical: 14),
-          
                   ),
                 ),
               ),
-          
               SizedBox(height: 16),
-          
               Container(
                 width: double.infinity,
                 height: 100,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(12),
-                  image: DecorationImage(image: NetworkImage(
-                    "https://images.unsplash.com/photo-1503376780353-7e6692767b70?w=1920",
-                    
-                   ),
-                  fit: BoxFit.cover, 
-                  
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://images.unsplash.com/photo-1503376780353-7e6692767b70?w=1920",
+                    ),
+                    fit: BoxFit.cover,
                   ),
                 ),
-          
-              )
-          
-          
-          
-                ],),
-        )),
+              ),
+              SizedBox(height: 16),
+              if (isLoading)
+                Center(child: CircularProgressIndicator())
+              else if (errorMessage != "")
+                Center(child: Text(errorMessage))
+              else
+                Expanded(
+                  child: GridView.builder(
+                    itemCount: allVehicles.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      childAspectRatio: 0.7,
+                    ),
+                    itemBuilder: (context, index) {
+                      final item = allVehicles[index];
+
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 5,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: ClipRRect(
+                                borderRadius: BorderRadiusGeometry.vertical(
+                                  top: Radius.circular(12),
+                                ),
+                                child: Image.network(
+                                  item.image ?? "",
+                                  width: double.infinity,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8.0, vertical: 6.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        child: Text(
+                                          item.make ?? "",
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 14,
+                                          ),
+                                        ),
+                                      ),
+                                      Text(
+                                        item.year?.toString() ?? "",
+                                        style: TextStyle(
+                                          color: Colors.grey,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 2),
+                                  Text(
+                                    item.model ?? "",
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      color: Colors.black45,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  SizedBox(height: 4),
+                                  Text(
+                                    item.description ?? "",
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      color: Colors.black54,
+                                      fontSize: 11,
+                                    ),
+                                  ),
+                                  SizedBox(height: 6),
+                                  Text(
+                                    item.price != null
+                                        ? "${item.currency ?? ""} ${item.price}"
+                                        : "N/A",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                      color: Colors.black87,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
