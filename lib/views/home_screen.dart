c04432e2dc5e6ef91378a7b9ecaf2bc6b838aba8
Berke Kara <kberkek00@gmail.com>
2026-03-09 17:42:50 +0300

    Add initial home screen layout

---
+++ lib/views/home_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+TextEditingController searchController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Discover", style: TextStyle(
+                    fontSize: 34,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: -0.5,
+                  ),
+                ),
+                IconButton(
+                  onPressed: (){}, 
+                  icon: Icon(Icons.shopping_bag_outlined),
+                  )
+                ],
+              ),
+              SizedBox(height: 8),
+          
+              Text(
+                  "Discover our latest vehicles.",
+                  style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey,
+                ),
+              ),
+          
+              SizedBox(height: 14),
+          
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xfff5f5f5),
+                  borderRadius: BorderRadius.circular(12), 
+                ),
+                child: TextField(
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    hintText: "Search vehicles",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    border: InputBorder.none,
+                    prefixIcon: Icon(Icons.search, color:Colors.grey),
+                    contentPadding: EdgeInsets.symmetric(vertical: 14),
+          
+                  ),
+                ),
+              ),
+          
+              SizedBox(height: 16),
+          
+              Container(
+                width: double.infinity,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(image: NetworkImage(
+                    "https://images.unsplash.com/photo-1503376780353-7e6692767b70?w=1920",
+                    
+                   ),
+                  fit: BoxFit.cover, 
+                  
+                  ),
+                ),
+          
+              )
+          
+          
+          
+                ],),
+        )),
+    );
+  }
+}
